77c3614bff9566d6693139e4f74a2823816248ab
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-11-10 11:30:37 +0300

    php mailing

--- mailing/mailingvariables.php
+++ mailing/mailingvariables.php
@@ -1,7 +1,7 @@
 <?php
     $mail_host = "smtp.gmail.com";
     $mail_port = "587";
-    $mail_sender_email = ""; //sender
-    $mail_sender_password = ""; //sender
+    $mail_sender_email = "comradesbiz@gmail.com"; //sender
+    $mail_sender_password = "imfbsyvawzmawknt"; //sender
     $mail_sender_name = "Website Form";
 ?>
